d1f9f3ee348f814e2e93bf3abe25a6de694d6f83
Rayhan Putra Randi <rayhanrandi10@gmail.com>
2022-09-22 08:37:12 +0700

    implement client restrictions

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from wishlist.views import register, login_user, logout_user
 from wishlist.views import show_wishlist, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
